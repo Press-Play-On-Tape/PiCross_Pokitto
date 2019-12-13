a3edbdf20c59d90312784416def86506151ad418
Stephane C <30958385+vampirics@users.noreply.github.com>
2019-12-13 09:14:21 -0500

    Update My_settings.h

--- My_settings.h
+++ My_settings.h
@@ -1,4 +1,5 @@
 #define PROJ_SCREENMODE 15
+#define PROJ_HIGH_RAM HIGH_RAM_MUSIC
 #define PROJ_ENABLE_SOUND       1       // 0 = all sound functions disabled
 #define PROJ_STREAMING_MUSIC    1       // 1 = enable streaming music from SD card
 #define PROJ_AUD_FREQ           11025
