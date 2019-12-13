56328aac4535c5afbb7eb6b6438ee65e89ad4d9f
Stephane C <30958385+vampirics@users.noreply.github.com>
2019-12-13 09:09:18 -0500

    Update My_settings.h

--- My_settings.h
+++ My_settings.h
@@ -1,5 +1,8 @@
 #define PROJ_SCREENMODE 15
-#define PROJ_ENABLE_SOUND 		1
-#define PROJ_STREAMING_MUSIC 	1
-#define PROJ_HIGH_RAM HIGH_RAM_MUSIC
+#define PROJ_ENABLE_SOUND       1       // 0 = all sound functions disabled
+#define PROJ_STREAMING_MUSIC    1       // 1 = enable streaming music from SD card
 #define PROJ_AUD_FREQ           11025
+#define PROJ_STREAM_TO_DAC      1       // 1 use DAC for stream, 0 = use PWM for stream
+#define PROJ_GBSOUND            0       // 1 = use Gamebuino-compatible sound interrupt 
+#define PROJ_ENABLE_SYNTH       0       // 1 = use Rboy-compatible sound interrupt
+#define PROJ_USE_PWM            0       // 0 = Use only DAC for output
